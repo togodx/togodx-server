00b1d06b7b48ebad44c57a493bc26fc7dd844faa
Toshiaki Katayama <k@bioruby.org>
2021-09-03 16:56:20 +0900

    Added migration files

---
+++ db/migrate/20210825070439_create_distributions.rb
@@ -0,0 +1,9 @@
+class CreateDistributions < ActiveRecord::Migration[6.1]
+  def change
+    create_table :distributions do |t|
+      t.string :distribution, null: false, index: true
+      t.string :distribution_label
+      t.float  :distribution_value, null: false, index: true
+    end
+  end
+end
