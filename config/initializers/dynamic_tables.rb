df89dec18d33a547f76580ebabc842832ebae7d4
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2021-09-06 03:25:07 +0900

    initial import

---
+++ config/initializers/dynamic_tables.rb
@@ -0,0 +1,14 @@
+Rails.configuration.to_prepare do
+  Attribute.all.each do |attribute|
+    const_name = "Table#{attribute.id}"
+
+    # undefine constant if it already exists (on reload, etc)
+    Object.send(:remove_const, const_name) if Object.const_defined? const_name
+
+    klass = Class.new(attribute.datamodel.constantize) do |klass|
+      klass.table_name = const_name.underscore
+    end
+
+    Object.const_set const_name, klass
+  end
+end
