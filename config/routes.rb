2706040124ff52f914e5fbc71e0f8f77dc633134
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2021-09-07 18:57:45 +0900

    refactor

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  match '/breakdown/:api', to: 'application#dispatch_breakdown', via: [:get, :post]
+  match '/breakdown/:api', to: 'application#breakdown', via: [:get, :post]
   match '/aggregate', to: 'application#aggregate', via: [:get, :post]
   match '/dataframe', to: 'application#dataframe', via: [:get, :post]
 end
