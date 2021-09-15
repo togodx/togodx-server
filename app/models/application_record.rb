ca9148fff6fbe76ab0f26816b837fa98877d7c3f
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2021-09-15 19:05:47 +0900

    add function to truncate table

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,16 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  class << self
+    def truncate_table
+      sql = case ActiveRecord::Base.connection.adapter_name
+            when 'SQLite'
+              "DELETE FROM #{table_name}; DELETE FROM sqlite_sequence WHERE name = '#{table_name}'; VACUUM;"
+            else
+              "TRUNCATE TABLE #{table_name};"
+            end
+
+      ActiveRecord::Base.connection.execute sql
+    end
+  end
 end
