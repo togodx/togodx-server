df0a4c12d494aff43dc20008b344f617f4e12a1b
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2021-10-16 18:24:26 +0900

    fix SQL that is not applicable for PostgreSQL

--- app/models/concerns/active_record_union.rb
+++ app/models/concerns/active_record_union.rb
@@ -2,8 +2,9 @@ module ActiveRecordUnion
   extend ActiveSupport::Concern
 
   class_methods do
-    def union(*relations)
-      from("(#{relations.map(&:to_sql).join(' UNION ')})")
+    def union(*relations, **options)
+      alias_name = options[:alias_name] || "t_#{self.table_name}"
+      from("(#{relations.map(&:to_sql).join(' UNION ')}) AS \"#{alias_name}\"")
     end
   end
 end
