7bc2802338fe8cf08728b76aea78d51ba9c1dbfa
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2022-01-20 15:34:28 +0900

    disable CORS if 0, f or false is set

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,4 +1,4 @@
-if ENV['RAILS_CORS_ENABLED'].present?
+if ENV['RAILS_CORS_ENABLED'].present? && !%w[0 f false].include?(ENV['RAILS_CORS_ENABLED'].downcase)
   Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
       origins '*'
